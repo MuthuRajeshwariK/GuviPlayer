bccd85e35c590a0edcd4daf57aafeebf56c0647c
MuthuRajeshwariK <muthurajeshwari.kcse@ksrce.ac.in>
2016-07-21 09:12:10 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,13 @@
+public class StringReverse {
+
+	
+	public static void main(String[] args)
+	{
+		String str="ksr";
+		int length=str.length();
+		for(int i=length-1;i>=0;i--)
+		{
+		System.out.print(str.charAt(i));
+		}
+	}
+}
