54567d069d41af1370de64b1db72407d7d0a3bb8
MuthuRajeshwariK <muthurajeshwari.kcse@ksrce.ac.in>
2016-07-22 10:26:20 +0530

    Create IdentifyPowerOf2.java

---
+++ IdentifyPowerOf2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IdentifyPowerOf2 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Enter a value:");
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		while(num!=2 && num%2==0)
+		{
+			num/=2;
+		}
+		if(num==2){
+			System.out.println("Given number is power of 2");
+		}
+		else{
+			System.out.println("Given number is not a power of 2");
+		}
+  s.close();
+	}
+
+}
