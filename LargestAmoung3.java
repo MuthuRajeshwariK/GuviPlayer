bbdc267ca488364cb5531894ad7abada830cbfe4
MuthuRajeshwariK <muthurajeshwari.kcse@ksrce.ac.in>
2016-07-22 10:43:46 +0530

    Create LargestAmoung3.java

---
+++ LargestAmoung3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LargestAmoung3 {
+
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a value:");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		if(a>b && a>c)
+		{
+			System.out.println(a+" is greater");
+		}
+		else if(b>c)
+		{
+			System.out.println(b+" is greater");
+		}
+		else{
+			System.out.println(c+" is greater");
+		}
+		s.close();
+	}
+
+}
