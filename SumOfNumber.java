954aab4d33fa0b7341da6782d97bd2514366e2d2
MuthuRajeshwariK <muthurajeshwari.kcse@ksrce.ac.in>
2016-08-10 14:25:51 +0530

    Create SumOfNumber.java

---
+++ SumOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumOfNumber {
+
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		int sum=0;
+		for(int i=1;i<=num;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
+		s.close();
+	}
+
+}
